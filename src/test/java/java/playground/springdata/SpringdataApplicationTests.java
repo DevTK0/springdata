f247dad1b78022e0489748397b1413b3160c4ec3
DevTK0 <tk0.developer.02@gmail.com>
2023-03-16 17:24:36 +0800

    First commit.

---
+++ src/test/java/java/playground/springdata/SpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package java.playground.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
